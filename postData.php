eb533cda1435299478f57f07d575090aebeb37e7
Tanveer Shams <tanveershams2218@gmail.com>
2022-08-27 11:17:43 +0600

    worked on reacts a bit

--- postData.php
+++ postData.php
@@ -4,38 +4,37 @@
 
         <div class="texthover" id="NameHeader" style="color: var(--col9); margin-bottom:-3%">
             <a href="" style="color: antiquewhite; text-decoration:none">
-                <img src="<?php echo $media->preview($posterUs['dp'],'dp') ?>" style="border-radius:50px; width:10%;">
+                <img src="<?php echo $media->preview($posterUs['dp'], 'dp') ?>" style="border-radius:50px; width:10%;">
             </a>
             <a href="" style="margin-left:2%; color: antiquewhite; text-decoration:none;">
-                <span class="texthover"><?php echo $posterUs['firstName']." ".$posterUs['lastName'] ?></span>
+                <span class="texthover"><?php echo $posterUs['firstName'] . " " . $posterUs['lastName'] ?></span>
             </a>
             <span class="textsizeCorrect" style="color: var(--col8); font-weight:normal">
                 <?php
                 $pron = 'their';
-                if($posterUs['gender']=="Male") {
+                if ($posterUs['gender'] == "Male") {
                     $pron = 'his';
-                } elseif($posterUs['gender']=="Female") {
+                } elseif ($posterUs['gender'] == "Female") {
                     $pron = 'her';
                 }
-                if($val['dp']) {
+                if ($val['dp']) {
                     echo " updated $pron profile picture";
-                } elseif($val['cover']) {
+                } elseif ($val['cover']) {
                     echo " updated $pron cover picture";
-                } elseif($val['hasImage']) {
+                } elseif ($val['hasImage']) {
                     echo " uploaded a picture";
                 } else {
                     echo " updated $pron status";
                 }
                 ?>
             </span>
-            <span class="smallestText" id="time"
-                style="margin-top:5%; color: var(--col8); float:right"><?php echo $val['date'] ?></span>
+            <span class="smallestText" id="time" style="margin-top:5%; color: var(--col8); float:right"><?php echo $val['date'] ?></span>
             <br>
         </div>
 
         <?php
 
-        if($posterUs['userID']==$_SESSION['user']) {
+        if ($posterUs['userID'] == $_SESSION['user']) {
             $postId = $val['postId'];
             echo "
                 <a href='editPost.php?postid=$postId' style='text-decoration:none'>
@@ -52,89 +51,94 @@
             <?php echo htmlspecialchars($val['post']) ?>
             <br> <br>
             <?php
-            if(file_exists($val['image'])) {
-                $image = $media->preview($val['image'],'dp');
-                if($wid=="prof") {
+            if (file_exists($val['image'])) {
+                $image = $media->preview($val['image'], 'dp');
+                if ($wid == "prof") {
                     echo "<img src='$image' style='width:45.5vw; margin-bottom:15px'/>";
                 } else {
                     echo "<img src='$image' style='width:48.5vw; margin-bottom:15px'/>";
                 }
             }
             ?>
-            <!--<?php
-
-            /*$likes = "";
-            $database = new connectDatabase();
-            $post = new createPosts();
-            $res = $post->getReactors($val['postId'], 'post');
-            $reacters = [];
-            $flag = false;
-            if ($res) {
-                $users = new userData();
-                foreach ($res as $value) {
-                    $valu = $users->fetchData($value['reactor']);
-                    $reacters[] = $valu['firstName'] . $valu['lastName'];
-                    if ($valu['userID'] == $_SESSION['user']) {
-                        $flag = true;
+            <?php ?> 
+            
+            <div class="textsizeCorrect" id="reactShow" style="margin-top: 2vh;padding-left: 4%;color:antiquewhite">
+            <?php
+                $likes = "";
+                $database = new connectDatabase();
+                $post = new createPosts();
+                $res = $post->getReactors($val['postId'], 'post');
+                $reacters = [];
+                $flag = false;
+                echo "<a id='reactors_$val[postId]' style='color:antiquewhite' href='showReactors.php?type=post&postid=$val[postId]'>";
+                if ($res) {
+                    $users = new userData();
+                    foreach ($res as $value) {
+                        $valu = $users->fetchData($value['reactor']);
+                        $reacters[] = $valu['firstName'] ." ". $valu['lastName'];
+                        if ($valu['userID'] == $_SESSION['user']) {
+                            $flag = true;
+                        }
+                    }
+                    $count = count($reacters);
+                    if ($flag) {
+                        $count = count($reacters) - 1;
+                        if ($count > 0) {
+                            if ($count == 1) {
+                                $likes = "You and 1 other liked this post.";
+                            } else {
+                                $likes = "You and " . $count . "others liked this post.";
+                            }
+                        } else {
+                            $likes = "You liked this post.";
+                        }
+                    } elseif ($count > 0) {
+                        $count = count($reacters) - 1;
+                        if ($count > 0) {
+                            if ($count == 1) {
+                                $likes = "$reacters[0] and 1 other liked this post.";
+                            } else {
+                                $likes = "$reacters[0] and " . $count . "others liked this post.";
+                            }
+                        } else {
+                            $likes = "$reacters[0] liked this post.";
+                        }
                     }
                 }
-                if ($flag) {
-                    $count = count($reacters) - 1;
-                    if ($count > 0) {
-                        if ($count == 1) {
-                            $likes = "You and 1 other liked this post.";
-                        }else {
-                            $likes = "You and " . $count . "others liked this post.";
-                        }
-                    }else {
-                        $likes = "You liked this post.";
-                    }
-                }elseif ($count > 0) {
-                    $count = count($reacters) - 1;
-                    if ($count > 0) {
-                        if ($count == 1) {
-                            $likes = "$reacters[0] and 1 other liked this post.";
-                        }else {
-                            $likes = "$reacters[0] and " . $count . "others liked this post.";
-                        }
-                    }else {
-                        $likes = "$reacters[0] liked this post.";
-                    }
-                }
-            }*/
 
+                if ($likes != "") {
+                    echo "<i class='fa fa-heart' style='padding-right: 5px;'>&nbsp$likes</i>";
+                } 
+                echo "</a>";
             ?>
-            <div id="flex" style="padding-left: 4%;">
-                <?php /*if ($likes != "") {
-                    echo "<i class='fa fa-heart' style='padding-right: 5px;font-size:1.7vh'>&nbsp$likes</i>";
-                } */?>
-            </div>-->
-            <div id="reactSec">
-                <div id="flex" style="padding-left: 15%;padding-right: 8%">
-                    <?php
-                    $reactCount = "";
-                    if ($val['reacts'] > 0) {
-                        $reactCount = "(" . $val['reacts'] . ")";
-                    }
-                    ?>
-                    <a onclick='getData(event)' href="react.php?type=post&postid=<?php echo $val['postId'] ?>" class="btn-with-hover" style="color: var(--col8); text-decoration:none;">
-                        <i class="fa fa-heart fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true">
-                            <?php echo $reactCount ?></a></i>
-                    </a>
-                </div>
-                <div id="flex"style="padding-left: 15%;padding-right: 8%;border-left: solid thin;">
-                    <a href="" class="btn-with-hover" style="color: var(--col8);">
-                        <i class="fa fa-comment fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true"></i>
-                    </a>
-                </div>
-                <div id="flex"style="padding-left: 15%;padding-right: 8%;border-left: solid thin;">
-                    <a href="" class="btn-with-hover" style="color: var(--col8);">
-                        <i class="fa fa-share fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true"></i>
-                    </a>
-                </div>
+                
+        </div>
+        <div id="reactSec">
+            <div id="flex" style="padding-left: 15%;padding-right: 8%">
+                <?php
+                $reactCount = "";
+                if ($val['reacts'] > 0) {
+                    $reactCount = "(" . $val['reacts'] . ")";
+                }
+                ?>
+                <a onclick='getData(event)' href="react.php?type=post&postid=<?php echo $val['postId'] ?>" class="btn-with-hover" style="color: var(--col8); text-decoration:none;">
+                    <i class="fa fa-heart fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true">
+                        <?php echo $reactCount ?></a></i>
+                </a>
+            </div>
+            <div id="flex" style="padding-left: 15%;padding-right: 8%;border-left: solid thin;">
+                <a href="" class="btn-with-hover" style="color: var(--col8);">
+                    <i class="fa fa-comment fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true"></i>
+                </a>
+            </div>
+            <div id="flex" style="padding-left: 15%;padding-right: 8%;border-left: solid thin;">
+                <a href="" class="btn-with-hover" style="color: var(--col8);">
+                    <i class="fa fa-share fa-2x" style="font-size:calc(1em + 0.5vw)" aria-hidden="true"></i>
+                </a>
             </div>
         </div>
     </div>
+</div>
 </div>
 
 <script type="text/javascript">
@@ -157,6 +161,7 @@
         data.ref = e.target.parentElement.href;
         reacter(data, e.target);
     }
+
     function response(res, tag) {
 
         if (res != "") {
@@ -168,7 +173,10 @@
                     reactCount = " (" + obj.react + ")";
                 }
                 tag.innerHTML = reactCount;
+                var post = document.getElementById(obj.postId);
+                post.innerHTML = obj.likes;
             }
         }
     }
+
 </script>
--- react.php
+++ react.php
@@ -1,33 +1,80 @@
-<?php 
+<?php
 
-    if(!isset($_SESSION['user'])) {
-        die;
+if (!isset($_SESSION['user'])) {
+    die;
+}
+
+$quer = explode("?", $data->ref);
+$quer = end($quer);
+
+
+$s = explode("&", $quer);
+
+foreach ($s as $val) {
+    $val = explode("=", $val);
+    $_GET[$val[0]] = $val[1];
+}
+if (isset($_GET['postid']) && isset($_GET['type']) && is_numeric($_GET['postid'])) {
+    $storeReact = new createPosts();
+    $ar[] = 'post';
+    $ar[] = 'comment';
+    $ar[] = 'friendRequests';
+    $ar[] = 'friendsCount';
+    if (in_array($_GET['type'], $ar)) {
+        $storeReact = new createPosts();
+        $storeReact->reactPost($_GET['postid'], $_GET['type'], $_SESSION['user']);
     }
-    
-    $quer = explode("?",$data->ref);
-    $quer = end($quer);
-    
-    
-    $s = explode("&",$quer);
-    
-    foreach($s as $val) {
-        $val = explode("=",$val);
-        $_GET[$val[0]] = $val[1];
-    }
-    if(isset($_GET['postid'])&& isset($_GET['type']) && is_numeric($_GET['postid'])) {
-       $storeReact = new createPosts();
-        $ar[] = 'post';
-        $ar[] = 'comment';
-        $ar[] = 'friendRequests';
-        $ar[] = 'friendsCount';
-        if(in_array($_GET['type'],$ar)) {
-            $storeReact = new createPosts();
-            $storeReact->reactPost($_GET['postid'],$_GET['type'],$_SESSION['user']);
+
+    $likes = $info = "";
+    $database = new connectDatabase();
+    $post = new createPosts();
+    $res = $post->getReactors($_GET['postid'], $_GET['type']);
+    $reacters = [];
+    $flag = false;
+    if ($res) {
+        $users = new userData();
+        foreach ($res as $value) {
+            $valu = $users->fetchData($value['reactor']);
+            $reacters[] = $valu['firstName'] ." ". $valu['lastName'];
+            if ($valu['userID'] == $_SESSION['user']) {
+                $flag = true;
+            }
         }
-
-        $reacts = $storeReact->getReactors($_GET['postid'],$_GET['type']);
-        $obj = (object)[];
-        $obj->react = count($reacts);
-        $obj->act = "reactPost";
-        echo json_encode($obj);
+        $count = count($reacters);
+        if ($flag) {
+            $count = count($reacters) - 1;
+            if ($count > 0) {
+                if ($count == 1) {
+                    $likes = "You and 1 other liked this post.";
+                } else {
+                    $likes = "You and " . $count . "others liked this post.";
+                }
+            } else {
+                $likes = "You liked this post.";
+            }
+        } elseif ($count > 0) {
+            $count = count($reacters) - 1;
+            if ($count > 0) {
+                if ($count == 1) {
+                    $likes = "$reacters[0] and 1 other liked this post.";
+                } else {
+                    $likes = "$reacters[0] and " . $count . "others liked this post.";
+                }
+            } else {
+                $likes = "$reacters[0] liked this post.";
+            }
+        }
     }
+
+    if ($likes != "") {
+        $likes =  "<i class='fa fa-heart' style='padding-right: 5px;font-size:1.7vh'>&nbsp$likes</i>";
+    }
+
+    $reacts = $storeReact->getReactors($_GET['postid'], $_GET['type']);
+    $obj = (object)[];
+    $obj->react = count($reacts);
+    $obj->act = "reactPost";
+    $obj->likes = $likes;
+    $obj->postId = "reactors_$_GET[postid]";
+    echo json_encode($obj);
+}
