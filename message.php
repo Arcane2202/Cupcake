270ca62dc92709a7930568518cf4189aecb287ed
Tanveer Shams <tanveershams2218@gmail.com>
2022-08-30 08:55:22 +0600

    humm

--- message.php
+++ message.php
@@ -3,9 +3,9 @@
     include_once "config.php";
     include("media.php");
     $media = new media();
-  if(!isset($_SESSION['user'])){
-    header("location: login.php");
-  }
+    if(!isset($_SESSION['user'])){
+        header("location: login.php");
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,13 +17,59 @@
     <title>Cupcake|Chatbox</title>
     <link rel="stylesheet" href="css/chat.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+    <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css" />
+<link rel="stylesheet" href="css/navStyle.css" />
 </head>
 
 <body class="textsizeCorrect">
+<!--
+<nav class="fixed-top">
+    <div class="menu-icon">
+        <span class="fas fa-bars"></span>
+    </div>
+
+
+    <a href="HomePage.php" class="basic">
+        <div class="logo floating">
+            Cupcake
+        </div>
+    </a>
+
+    <div class="nav-items">
+        <li class="li_icons"><a href="getMessagePage.php" class="basic icons"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
+        <li class="li_icons"><a href="" class="basic icons"><i class="fa fa-bell" aria-hidden="true"></i></a></li>
+        <li class="li_img"><a href="ProfilePage.php">
+                <img src=<?php echo $media->preview($userData['dp'],'dp') ?> class="propic" id="profilepic" alt="profilepic">
+            </a>
+
+            <div class="dropdown">
+                <ul>
+                    <li class="dropdown-link"> <a href="ProfilePage.php"> Profile <i class="fa fa-user stickers" aria-hidden="true"></i></a> </li>
+                    <li class="dropdown-link"> <a href="./ContactForm.php"> Help <i class="fa fa-question-circle stickers" aria-hidden="true"></i></a> </li>
+                    <li class="dropdown-link"> <a href="logout.php"> Logout <i class="fa fa-sign-out stickers" aria-hidden="true"></i></a> </li>
+                </ul>
+            <div>
+
+        </li>
+    </div>
+    <div class="search-icon">
+        <span class="fas fa-search"></span>
+    </div>
+    <div class="cancel-icon">
+        <span class="fas fa-times"></span>
+    </div>
+
+    <form action="#">
+        <input type="search" class="search-data" placeholder="Search" required>
+        <button type="submit" class="fas fa-search"></button>
+    </form>
+       
+</nav>
+-->
 <div id="bodyContainer">
     <div class="wrapper">
         <section class="chat-area">
-            <header>
+            <a style="text-decoration:none;color:antiquewhite;" href="ProfilePage.php?id=<?php echo $_GET['id']; ?>"><header>
                 <?php 
           
           $user_id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -34,12 +80,12 @@
             header("location: users.php");
           }
         ?>
-           
+                
                 <img src="<?php echo $media->preview($row['dp'], 'dp') ?>" style="border-radius:50%; width:5%;">
                 <div class="details" style="margin-left:1%">
                     <span><?php echo $row['firstName']. " " . $row['lastName'] ?></span>
                 </div>
-            </header>
+            </header></a>
             <div class="chat-box">
 
             </div>
@@ -119,6 +165,37 @@
         chatBox.scrollTop = chatBox.scrollHeight;
     }
     </script>
+
+<script>
+    const menuBtn = document.querySelector(".menu-icon span");
+    const searchBtn = document.querySelector(".search-icon");
+    const cancelBtn = document.querySelector(".cancel-icon");
+    const items = document.querySelector(".nav-items");
+    const form = document.querySelector("form");
+    const pp = document.querySelector(".propic");
+    const dropdown = document.querySelector(".dropdown");
+   
+    menuBtn.onclick = () => {
+        items.classList.add("active");
+        menuBtn.classList.add("hide");
+        searchBtn.classList.add("hide");
+        cancelBtn.classList.add("show");
+    }
+
+    cancelBtn.onclick = () => {
+        items.classList.remove("active");
+        menuBtn.classList.remove("hide");
+        searchBtn.classList.remove("hide");
+        cancelBtn.classList.remove("show");
+        form.classList.remove("active");
+    }
+    searchBtn.onclick = () => {
+        form.classList.add("active");
+        searchBtn.classList.add("hide");
+        cancelBtn.classList.add("show");
+    }
+
+</script>
 
 </body>
 
