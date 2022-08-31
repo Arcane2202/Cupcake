d7b699eb546d15bc86956559c7dbe6ef1727bd9c
Tanveer Shams <tanveershams2218@gmail.com>
2022-08-31 11:47:27 +0600

    adminDataFetch

--- adminpanel.php
+++ adminpanel.php
@@ -1,5 +1,29 @@
+<?php
+include("connect.php");
+include("userInformation.php");
+$user = "";
+$query = "SELECT * FROM users";
+$db = new connectDatabase();
+$us = $db->read($query);
+$query = "SELECT * FROM posts";
+$res2 = $db->read($query);
+$query = "SELECT * FROM comments";
+$res3 = $db->read($query);
+$query = "SELECT * FROM reacts";
+$res4 = $db->read($query);
+$user = count($us);
+$post = count($res2);
+$comment = count($res3);
+$react = count($res4);
+$quer = "SELECT * FROM support";
+$res = $db->read($quer);
+$cnt=0;
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +31,16 @@
     <link rel="icon" type="img/svg" href="img/cupcake-wh.svg">
     <link rel="stylesheet" href="css/admin.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.css">
-        
-    
+
+
     <title>Cupcake|Admin Panel</title>
 </head>
+
 <body>
     <div class="side-menu">
         <div class="brand-name ">
-            <h1 class="tubelight" >Cupcake</h1>
-            <h3 class="tubelight" style="font-size:20px;">Admin Panel</h3>              
+            <h1 class="tubelight">Cupcake</h1>
+            <h3 class="tubelight" style="font-size:20px;">Admin Panel</h3>
         </div>
 
         <ul class="ul-spacing">
@@ -39,7 +64,7 @@
             <div class="cards">
                 <div class="card">
                     <div class="box">
-                        <h1>3440</h1>
+                        <h1><?php echo $user ?></h1>
                         <h3>USERS</h3>
                     </div>
                     <div class="icon-case">
@@ -49,7 +74,7 @@
 
                 <div class="card">
                     <div class="box">
-                        <h1>9600</h1>
+                        <h1><?php echo $post ?></h1>
                         <h3>POSTS</h3>
                     </div>
                     <div class="icon-case">
@@ -59,7 +84,7 @@
 
                 <div class="card">
                     <div class="box">
-                        <h1>14000</h1>
+                        <h1><?php echo $react ?></h1>
                         <h3>REACTS</h3>
                     </div>
                     <div class="icon-case">
@@ -69,7 +94,7 @@
 
                 <div class="card">
                     <div class="box">
-                        <h1>6000</h1>
+                        <h1><?php echo $comment ?></h1>
                         <h3>COMMENTS</h3>
                     </div>
                     <div class="icon-case">
@@ -92,6 +117,21 @@
                             <th>Phone Number</th>
                             <th>Message</th>
                         </tr>
+                        <?php
+                        foreach ($res as $valluk) {
+                        ?>
+                            <tr>
+
+
+                                <td><?php echo $valluk['userid'] ?></td>
+                                <td><?php echo $valluk['name'] ?></td>
+                                <td><?php echo $valluk['email'] ?></td>
+                                <td><?php echo $valluk['phone'] ?></td>
+                                <td><?php echo $valluk['message'] ?></td>
+
+
+                            </tr>
+                        <?php } ?>
                     </table>
                 </div>
                 <div class="new-users">
@@ -99,6 +139,49 @@
                         <h2> New Users </h2>
                         <a href="#" class="btn">View all</a>
                     </div>
+                    <table>
+                        <tr>
+                            <th>User ID</th>
+                            <th>User Name</th>
+                            <th>User E-Mail</th>
+                            <th>User Phone Number</th>
+                            <th>User Gender</th>
+                            <th>User Friend Count</th>
+                            <th>User Friend Request</th>
+                            <th>User React Count</th>
+                            <th>User Join Date</th>
+                        </tr>
+                        <?php
+                        foreach ($us as $valluk) {
+                        ?>
+                            <tr>
+
+
+                                <td><?php echo $valluk['userID'] ?></td>
+                                <td><?php echo $valluk['firstName'] . " " . $valluk['lastName'] ?></td>
+                                <td><?php echo $valluk['email'] ?></td>
+                                <td><?php echo $valluk['phone'] ?></td>
+                                <td><?php echo $valluk['gender'] ?></td>
+                                <td><?php echo $valluk['friendsCount'] ?></td>
+                                <td><?php echo $valluk['friendRequests'] ?></td>
+                                <?php
+                                $quer = "SELECT reacts FROM reacts";
+                                $ress = $db->read($quer);
+                                $resAr = json_decode($ress[0]['reacts'], true);
+                                $cnt = 0;
+                                foreach ($resAr as $value) {
+                                    if ($value['reactor'] == $valluk['userID']) {
+                                        $cnt++;
+                                    }
+                                }
+                                ?>
+                                <td><?php echo $cnt ?></td>
+                                <td><?php echo $valluk['date'] ?></td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                    
+
                 </div>
             </div>
         </div>
@@ -127,7 +210,7 @@
         feedbacks.classList.remove("show");
         users.classList.remove("show");
         users.classList.add("hide");
-    
+
         Dashboard.onclick = () => {
             Feedback.classList.remove("active");
             Handle.classList.remove("active");
@@ -167,7 +250,6 @@
             users.classList.remove("hide");
             users.classList.add("show");
         }
-
     </script>
 </body>
 
